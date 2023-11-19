5c8c6500fa2907b6c752cf102bbca655cf5b71f7
Kishanbhadoriya007 <kishanbhadoriya4123@gmail.com>
2023-11-19 17:27:57 +0530

    improved code

--- supplier.py
+++ supplier.py
@@ -14,19 +14,11 @@ class SupplierClass:
         self.var_searchby=StringVar()
         self.var_searchtxt=StringVar()
 
-        self.var_emp_id=StringVar()
-        self.var_gender=StringVar()
+        self.var_sup_id=StringVar()
         self.var_contact=StringVar()
         self.var_name=StringVar()
-        self.var_dob=StringVar()
-        self.var_doj=StringVar()
-        self.var_email=StringVar()
-        self.var_pass=StringVar()
-        self.var_utype=StringVar()
-        self.var_salary=StringVar()
-
         #====searchFrame=====
-        SearchFrame=LabelFrame(self.root,text="Search Employee",bg="White")
+        SearchFrame=LabelFrame(self.root,text="Search supplier",bg="White")
         SearchFrame.place(x=250,y=20,width=600,height=70)
 
         #====options====
@@ -40,27 +32,21 @@ class SupplierClass:
         btn_search=Button(SearchFrame,text="Search",command=self.search,font=("goudy old style",15),bg="#4caf49",fg="white",cursor="hand2").place(x=410,y=10,width=140,height=30)
 
         #====text====
-        title=Label(self.root,text="Employee Details",font=("Goudy old style",15),bg ="blue",fg="white").place(x=50,y=100,width=1000)
+        title=Label(self.root,text="supplier Details",font=("Goudy old style",15),bg ="blue",fg="white").place(x=50,y=100,width=1000)
 
         #*****Content*****
         #*****Row1*******
-        lbl_empid=Label(self.root,text="Emp ID",font=("Goudy old style",15),bg ="White").place(x=50,y=150)
-        lbl_gender=Label(self.root,text="Gender",font=("Goudy old style",15),bg ="White").place(x=430,y=150)
+        lbl_supid=Label(self.root,text="Emp ID",font=("Goudy old style",15),bg ="White").place(x=50,y=150)
         lbl_contact=Label(self.root,text="Contact",font=("Goudy old style",15),bg ="White").place(x=800,y=150)
 
-        cmb_gender=ttk.Combobox(self.root,textvariable=self.var_gender, values=("Select","male","female","other"),state='readonly',justify=CENTER,font=("times new roman",15))
-        cmb_gender.place(x=500,y=150,width=180)
-        cmb_gender.current(0)
 
-        txt_empid=Entry(self.root,textvariable=self.var_emp_id,font=("Goudy old style",15),bg ="lightblue").place(x=120,y=150,width=180)
+        txt_supid=Entry(self.root,textvariable=self.var_emp_id,font=("Goudy old style",15),bg ="lightblue").place(x=120,y=150,width=180)
         txt_contact=Entry(self.root,textvariable=self.var_contact,font=("Goudy old style",15),bg="lightblue").place(x=870,y=150,width=180)
 
         #******Row2*******
 
         lbl_name=Label(self.root,text="Name",font=("Goudy old style",15),bg ="White").place(x=50,y=190)
-        lbl_dob=Label(self.root,text="D.O.B",font=("Goudy old style",15),bg ="White").place(x=430,y=190)
-        lbl_doj=Label(self.root,text="D.O.J",font=("Goudy old style",15),bg ="White").place(x=800,y=190)
-
+       
         txt_name=Entry(self.root,textvariable=self.var_name,font=("Goudy old style",15),bg ="lightblue").place(x=120,y=190,width=180)
         txt_dob=Entry(self.root,textvariable=self.var_dob,font=("Goudy old style",15),bg="lightblue").place(x=500,y=190,width=180)
         txt_doj=Entry(self.root,textvariable=self.var_doj,font=("Goudy old style",15),bg="lightblue").place(x=870,y=190,width=180)
@@ -94,7 +80,7 @@ class SupplierClass:
         btn_delete=Button(self.root,text="Delete",command=self.delete,font=("goudy old style",15),bg="red",fg="white",cursor="hand2").place(x=740,y=305,width=110,height=30)
         btn_clear=Button(self.root,text="Clear",command=self.clear,font=("goudy old style",15),bg="grey",fg="white",cursor="hand2").place(x=860,y=305,width=110,height=30)
 
-        #*****Details of employees****
+        #*****Details of suppliers****
 
         emp_frame=Frame(self.root,bd=3,relief=RIDGE)
         emp_frame.place(x=0,y=350,relwidth=1,height=150)
@@ -102,38 +88,38 @@ class SupplierClass:
         scrolly=Scrollbar(emp_frame,orient=VERTICAL)
         scrollx=Scrollbar(emp_frame,orient=HORIZONTAL)
 
-        self.EmployeeTable=ttk.Treeview(emp_frame,columns=("eid","name","email","gender","contact","dob","doj","pass","utype","address","salary"),yscrollcommand=scrolly.set,xscrollcommand=scrollx.set)
+        self.supplierTable=ttk.Treeview(emp_frame,columns=("eid","name","email","gender","contact","dob","doj","pass","utype","address","salary"),yscrollcommand=scrolly.set,xscrollcommand=scrollx.set)
         scrollx.pack(side=BOTTOM,fill=X)
         scrolly.pack(side=RIGHT,fill=Y)
-        scrollx.config(command=self.EmployeeTable.xview)
-        scrolly.config(command=self.EmployeeTable.yview)
-        self.EmployeeTable.heading("eid", text="EMP ID")
-        self.EmployeeTable.heading("name", text="Name")
-        self.EmployeeTable.heading("email", text="Email")
-        self.EmployeeTable.heading("gender", text="Gender")
-        self.EmployeeTable.heading("contact", text="Contact")
-        self.EmployeeTable.heading("dob", text="D.O.B")
-        self.EmployeeTable.heading("doj", text="D.O.J")
-        self.EmployeeTable.heading("pass", text="Password")
-        self.EmployeeTable.heading("utype", text="User Type")
-        self.EmployeeTable.heading("address", text="Address")
-        self.EmployeeTable.heading("salary", text="Salary")
+        scrollx.config(command=self.supplierTable.xview)
+        scrolly.config(command=self.supplierTable.yview)
+        self.supplierTable.heading("eid", text="EMP ID")
+        self.supplierTable.heading("name", text="Name")
+        self.supplierTable.heading("email", text="Email")
+        self.supplierTable.heading("gender", text="Gender")
+        self.supplierTable.heading("contact", text="Contact")
+        self.supplierTable.heading("dob", text="D.O.B")
+        self.supplierTable.heading("doj", text="D.O.J")
+        self.supplierTable.heading("pass", text="Password")
+        self.supplierTable.heading("utype", text="User Type")
+        self.supplierTable.heading("address", text="Address")
+        self.supplierTable.heading("salary", text="Salary")
 
-        self.EmployeeTable["show"]="headings"
+        self.supplierTable["show"]="headings"
 
-        self.EmployeeTable.column("eid", width=90)
-        self.EmployeeTable.column("name", width=100)
-        self.EmployeeTable.column("email", width=100)
-        self.EmployeeTable.column("gender", width=100)
-        self.EmployeeTable.column("contact", width=100)
-        self.EmployeeTable.column("dob", width=100)
-        self.EmployeeTable.column("doj", width=100)
-        self.EmployeeTable.column("pass", width=100)
-        self.EmployeeTable.column("utype", width=100)
-        self.EmployeeTable.column("address", width=100)
-        self.EmployeeTable.column("salary", width=200)
-        self.EmployeeTable.pack(fill=BOTH,expand=1)
-        self.EmployeeTable.bind("<ButtonRelease-1>",self.get_data)
+        self.supplierTable.column("eid", width=90)
+        self.supplierTable.column("name", width=100)
+        self.supplierTable.column("email", width=100)
+        self.supplierTable.column("gender", width=100)
+        self.supplierTable.column("contact", width=100)
+        self.supplierTable.column("dob", width=100)
+        self.supplierTable.column("doj", width=100)
+        self.supplierTable.column("pass", width=100)
+        self.supplierTable.column("utype", width=100)
+        self.supplierTable.column("address", width=100)
+        self.supplierTable.column("salary", width=200)
+        self.supplierTable.pack(fill=BOTH,expand=1)
+        self.supplierTable.bind("<ButtonRelease-1>",self.get_data)
 
         self.show()
 
@@ -144,14 +130,14 @@ class SupplierClass:
         cur=con.cursor();
         try:
             if self.var_emp_id.get()=="":
-                messagebox.showerror("Error","Employee ID must be required",parent=self.root)
+                messagebox.showerror("Error","supplier ID must be required",parent=self.root)
             else:
-                cur.execute("Select * from employee where eid=?",(self.var_emp_id.get(),))
+                cur.execute("Select * from supplier where eid=?",(self.var_emp_id.get(),))
                 row=cur.fetchone()
                 if row!=None:
-                    messagebox.showerror("Error","This employee ID already assigned, try different",parent=self.root)
+                    messagebox.showerror("Error","This supplier ID already assigned, try different",parent=self.root)
                 else:
-                    cur.execute("Insert into employee (eid,name,email,gender,contact,dob,doj,pass,utype,address,salary) values(?,?,?,?,?,?,?,?,?,?,?)",(
+                    cur.execute("Insert into supplier (eid,name,email,gender,contact,dob,doj,pass,utype,address,salary) values(?,?,?,?,?,?,?,?,?,?,?)",(
                         self.var_emp_id.get(),
                         self.var_name.get(),
                         self.var_email.get(),
@@ -165,7 +151,7 @@ class SupplierClass:
                         self.var_salary.get(),
                     ))
                     con.commit()
-                    messagebox.showinfo("Success","Employee added successfully",parent=self.root)
+                    messagebox.showinfo("Success","supplier added successfully",parent=self.root)
                     self.show()
         except Exception as ex:
             messagebox.showerror("Error",f"Error due to : {str(ex)}")
@@ -175,18 +161,18 @@ class SupplierClass:
         con=sqlite3.connect(database=r'ims.db')
         cur=con.cursor();
         try:
-            cur.execute("Select * from employee")
+            cur.execute("Select * from supplier")
             rows=cur.fetchall()
-            self.EmployeeTable.delete(*self.EmployeeTable.get_children())
+            self.supplierTable.delete(*self.supplierTable.get_children())
             for row in rows:
-                self.EmployeeTable.insert('',END,values=row)
+                self.supplierTable.insert('',END,values=row)
 
         except Exception as ex:
             messagebox.showerror("Error",f"Error due to : {str(ex)}")
 #******Get-data******
     def get_data(self,ev):
-        f=self.EmployeeTable.focus()
-        content=(self.EmployeeTable.item(f))
+        f=self.supplierTable.focus()
+        content=(self.supplierTable.item(f))
         row=content['values']
         #print(row)
         self.var_emp_id.set(row[0])
@@ -207,14 +193,14 @@ class SupplierClass:
         cur=con.cursor();
         try:
             if self.var_emp_id.get()=="":
-                messagebox.showerror("Error","Employee ID must be required",parent=self.root)
+                messagebox.showerror("Error","supplier ID must be required",parent=self.root)
             else:
-                cur.execute("Select * from employee where eid=?",(self.var_emp_id.get(),))
+                cur.execute("Select * from supplier where eid=?",(self.var_emp_id.get(),))
                 row=cur.fetchone()
                 if row==None:
-                    messagebox.showerror("Error","Invalid Employee ID",parent=self.root)
+                    messagebox.showerror("Error","Invalid supplier ID",parent=self.root)
                 else:
-                    cur.execute("Update employee set name=?,email=?,gender=?,contact=?,dob=?,doj=?,pass=?,utype=?,address=?,salary=? where eid=?",(
+                    cur.execute("Update supplier set name=?,email=?,gender=?,contact=?,dob=?,doj=?,pass=?,utype=?,address=?,salary=? where eid=?",(
                                                 self.var_name.get(),
                                                 self.var_email.get(),
                                                 self.var_gender.get(),
@@ -229,7 +215,7 @@ class SupplierClass:
                                                      
                     ))
                     con.commit()
-                    messagebox.showinfo("Success","Employee Updated successfully",parent=self.root)
+                    messagebox.showinfo("Success","supplier Updated successfully",parent=self.root)
                     self.show()
         except Exception as ex:
             messagebox.showerror("Error",f"Error due to : {str(ex)}")
@@ -239,18 +225,18 @@ class SupplierClass:
         cur=con.cursor();
         try:
             if self.var_emp_id.get()=="":
-                messagebox.showerror("Error","Employee ID must be required",parent=self.root)
+                messagebox.showerror("Error","supplier ID must be required",parent=self.root)
             else:
-                cur.execute("Select * from employee where eid=?",(self.var_emp_id.get(),))
+                cur.execute("Select * from supplier where eid=?",(self.var_emp_id.get(),))
                 row=cur.fetchone()
                 if row==None:
-                    messagebox.showerror("Error","Invalid Employee ID",parent=self.root)
+                    messagebox.showerror("Error","Invalid supplier ID",parent=self.root)
                 else:
                     op=messagebox.askyesno("Confirm","Do you really want to delete",parent=self.root)
                     if op==True:
-                        cur.execute("delete from employee where eid=?",(self.var_emp_id.get(),))
+                        cur.execute("delete from supplier where eid=?",(self.var_emp_id.get(),))
                         con.commit()
-                        messagebox.showinfo("Delete","Employee Deleted Deleted Successfully",parent=self.root)
+                        messagebox.showinfo("Delete","supplier Deleted Deleted Successfully",parent=self.root)
                         self.clear()
             
 
@@ -282,12 +268,12 @@ class SupplierClass:
             elif self.var_searchtxt.get()=="":
                 messagebox.showerror("Error","Search Input is required",parent=self.root)
             else:
-                cur.execute("select * from employee where " + self.var_searchby.get() + " LIKE '%" + self.var_searchtxt.get() + "%'")
+                cur.execute("select * from supplier where " + self.var_searchby.get() + " LIKE '%" + self.var_searchtxt.get() + "%'")
                 rows=cur.fetchall()
                 if len(rows)!=0:
-                    self.EmployeeTable.delete(*self.EmployeeTable.get_children())
+                    self.supplierTable.delete(*self.supplierTable.get_children())
                     for row in rows:
-                        self.EmployeeTable.insert('',END,values=row)
+                        self.supplierTable.insert('',END,values=row)
                 else:
                     messagebox.showerror("Error","No record found!!!",parent=self.root)
         except Exception as ex:
